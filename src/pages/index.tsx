65b64a808ee3e1a5b93a477c7041153291eb1bb9
Anaportfolio <biamaiaribeiro@gmail.com>
2025-05-29 14:50:02 -0300

    Created table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Layout from "./Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34,'1'),
+    new Cliente('Maria', 22,'2'),
+    new Cliente('Luana', 19,'3')
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente){
+    console.log(cliente.nome)
+  }
   return (
     <div className= {`
         flex justify-center items-center h-screen
@@ -9,7 +25,7 @@ export default function Home() {
     `}>
 
       <Layout title={"Conteudo Simples"}>
-        <span>Conteudo</span>
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
       </Layout>
 
     </div>
